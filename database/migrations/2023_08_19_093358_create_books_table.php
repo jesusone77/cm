64897613dc29c8a258b58a45bc62bd89e5e74132
Jesus Emmanuel Baca Hernández <jesus.e.b.h.one@gmail.com>
2023-08-19 04:49:16 -0600

    tenemos la base creada con eloquent

---
+++ database/migrations/2023_08_19_093358_create_books_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('no_pages');
+            $table->foreign('id')->references('id')->on('autors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
