f24e00157ab2923a41932e20f9405d919c12115d
Jesus Emmanuel Baca Hernández <jesus.e.b.h.one@gmail.com>
2023-08-19 18:48:03 -0600

    Make Controller, Request and declare Model for Book

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ 
+// Route::resource('books', BookController::class)
+//     ->missing(function (Request $request) {
+//     return Redirect::route('books.index');
+// });
